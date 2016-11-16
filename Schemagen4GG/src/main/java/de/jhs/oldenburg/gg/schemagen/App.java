e563e3ff28a7dba8904c3d84760f10be23aea4cd
SSA_Hernst <SSA_Hernst>
2016-11-16 14:28:22 +0100

    hello

---
+++ Schemagen4GG/src/main/java/de/jhs/oldenburg/gg/schemagen/App.java
@@ -0,0 +1,13 @@
+package de.jhs.oldenburg.gg.schemagen;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
