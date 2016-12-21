66ddafd3924ce394ad6a4be646c40363be003c94
Philipp <Philipp@Fipsy>
2016-12-21 20:09:43 +0100

    OWLTDBManager Named graphes Select Tripels

--- RDF_Creation/src/de/jhs/oldenburg/gg/tdb/manager/ITDBManager.java
+++ RDF_Creation/src/de/jhs/oldenburg/gg/tdb/manager/ITDBManager.java
@@ -4,12 +4,10 @@ import java.io.File;
 
 import org.apache.jena.query.Dataset;
 import org.apache.jena.rdf.model.Model;
+import org.apache.jena.riot.Lang;
 
 public interface ITDBManager {
 
 	public Dataset createOrConnectTDB(String datasetName, String datasetLocation);
-	public boolean insertDataFromFile(File file, String fileType);
-	public Model getModelfromTDB();
-	public String printRDFContent();
-	public String makeSPARQLReq(String query);
+	public boolean insertDataFromFile(File file, Lang lang);
 }
--- RDF_Creation/src/de/jhs/oldenburg/gg/tdb/manager/TDBManager.java
+++ RDF_Creation/src/de/jhs/oldenburg/gg/tdb/manager/TDBManager.java
@@ -2,6 +2,8 @@ package de.jhs.oldenburg.gg.tdb.manager;
 
 import java.io.File;
 
+import javax.transaction.TransactionRequiredException;
+
 import org.apache.jena.query.Dataset;
 import org.apache.jena.query.Query;
 import org.apache.jena.query.QueryExecution;
@@ -18,9 +20,11 @@ import org.apache.jena.rdf.model.RDFNode;
 import org.apache.jena.rdf.model.Resource;
 import org.apache.jena.rdf.model.Statement;
 import org.apache.jena.rdf.model.StmtIterator;
-import org.apache.jena.rdf.model.impl.ResourceImpl;
+import org.apache.jena.riot.Lang;
 import org.apache.jena.riot.RDFDataMgr;
 import org.apache.jena.tdb.TDBFactory;
+
+import de.jhs.oldenburg.gg.tdb.utils.TDBPrinter;
 
 /**
  * 
@@ -31,19 +35,9 @@ public class TDBManager implements ITDBManager {
 
 	private Dataset dataset;
 
-	public static void main(String[] args) {
-		TDBManager a = new TDBManager();
-		a.createOrConnectTDB("TestFolder", "myTDB");
-		// Insert SCHEMA into database
-		File f = new File("RDF_Files/friends.owl");
-		//
-		// System.out.println(f.getAbsoluteFile().getAbsolutePath());
-		a.insertDataFromFile(f, "OWL");
-		// a.printRDFContent();
-		// a.makeSPARQLReq("SELECT  *  {?s ?p ?o} ");
-		a.resolveCompound("prefix pers: <file:///C:/Users/Philipp/git/Project-TripleStoreDataBase/RDF_Creation/RDF_Files/> select  distinct ?subject where { ?subject a ?o.}");
-	}
-
+	/**
+	 * 
+	 */
 	@Override
 	public Dataset createOrConnectTDB(String datasetLocation, String datasetName) {
 		String dir = datasetLocation + "/" + datasetName;
@@ -55,33 +49,98 @@ public class TDBManager implements ITDBManager {
 		// erstelle ein dataset
 		Dataset dataset = TDBFactory.createDataset(datasetLocation + "/" + datasetName);
 		this.dataset = dataset;
-		// System.out.println(dataset.getContext());
+		System.out.println(dataset.getContext());
 		return dataset;
 	}
 
+	/**
+	 * 
+	 */
 	@Override
-	public boolean insertDataFromFile(File file, String fileType) {
-		boolean transactionSucess = false;
+	public boolean insertDataFromFile(File file, Lang lang) {
 		try {
-			Model model = RDFDataMgr.loadModel(file.getAbsolutePath());
-			Model m = dataset.getDefaultModel().add(model);
-			transactionSucess = true;
+			Model m = RDFDataMgr.loadModel(file.getAbsolutePath(), lang);
+			dataset.getDefaultModel().add(m);
+			System.out.println("LoadModel");
 		} catch (NullPointerException e) {
 			e.printStackTrace();
 		}
-		return transactionSucess;
+		return true;
 	}
 
+	/**
+	 * 
+	 * @return
+	 */
+	public boolean resolveCompoundTEST1() {
+		boolean result = false;
+		// building the query
+		Query query = QueryFactory.create("select distinct ?s ?x ?o where { ?s ?x ?o.}");
+		// Query query2 =
+		// QueryFactory.create("prefix jhs: <http://www.jade-hs.de> CONSTRUCT { <hhttp://www.jade-hs.de/Existenzbedingung/Existenzbedingung_4> jhs:Name ?name } WHERE"
+		// + "{ ?x jhs:name ?name }");
+		// requesting the compound instance
+		String baseURI = query.getBaseURI();
+		System.out.println(baseURI);
+		if (dataset != null) {
+			dataset.begin(ReadWrite.READ);
+			// get the existing model
+			Model model = dataset.getDefaultModel();
+			// Execute query
+			try (QueryExecution qexec = QueryExecutionFactory.create(query, model)) {
+				ResultSet results = qexec.execSelect();
+
+				for (; results.hasNext();) {
+					QuerySolution soln = results.nextSolution();
+					RDFNode s = soln.get("?s");
+					RDFNode o = soln.get("?o");
+					if (s.isLiteral())
+						((Literal) s).getLexicalForm();
+					else if (s.isResource()) {
+						Resource r = (Resource) s;
+						// System.out.println(r);
+						if (!r.isAnon()) {
+							r.getURI();
+						}
+					}
+					if (o.isResource()) {
+						Resource r2 = (Resource) o;
+						// System.out.println(r2);
+					}
+				}
+			} finally {
+				dataset.end();
+			}
+		} else
+			System.out.println("Dataset = null");
+		// resolve answer
+		return result;
+	}
+
+	public void resolveCompoundTEST2() {
+
+		// ///Query to the Graph model//////
+
+		// ///Get result//////
+		// ///Recursive processing//////
+
+	}
+
+	
+
+	/**
+ 	 * 
+	 */
 	public void deleteModel() {
 		if (dataset != null) {
 			Model m = dataset.getDefaultModel();
-			m.remove(m);
+			m.removeAll();
 		} else {
 			System.out.println("Cannot find any RDF-model!");
 		}
-
 	}
 
+	// ====Getters and Setters
 	/**
 	 * 
 	 * @return
@@ -98,108 +157,20 @@ public class TDBManager implements ITDBManager {
 		this.dataset = dataset;
 	}
 
-	@Override
-	public Model getModelfromTDB() {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
-	@Override
-	public String printRDFContent() {
-		Model m = dataset.getDefaultModel();
-		StmtIterator iter = m.listStatements();
-		// print out the predicate, subject and object of each statement
-		while (iter.hasNext()) {
-			Statement stmt = iter.nextStatement(); // get next statement
-			Resource subject = stmt.getSubject(); // get the subject
-			Property predicate = stmt.getPredicate(); // get the predicate
-			RDFNode object = stmt.getObject(); // get the object
-			//
-			System.out.print("Subject( " + subject.toString() + " ) -->");
-			System.out.print(" predicate( " + predicate.toString() + " ) -->");
-			if (object instanceof Resource) {
-				System.out.print(" object( " + object.toString() + " )");
-			} else {
-				// object is a literal
-				System.out.print(" ObjectLiteral( " + object.toString() + " )");
-			}
-		}
-		return null;
-	}
-
 	/**
 	 * 
-	 * @return
+	 * @param args
 	 */
-	public boolean resolveCompound(String queryString) {
-		boolean result = false;
-		// building the query
-
-		Query query = QueryFactory.create(queryString);
-		// requesting the compound instance
-		if (dataset != null) {
-			dataset.begin(ReadWrite.READ);
-			// get the existing model
-			Model model = dataset.getDefaultModel();
-			// Execute query
-			try (QueryExecution qexec = QueryExecutionFactory.create(query, model)) {
-				// StmtIterator it =
-				ResultSet results = qexec.execSelect();
-				// Model m = qexec.execConstruct();
-
-				for (; results.hasNext();) {
-					QuerySolution soln = results.nextSolution();
-					// Access variables: 
-					RDFNode s = soln.get("?subject"); //
-					RDFNode c = soln.get("?subject"); //
-														
-					// If you need to test the thing returned
-					if (s.isLiteral())
-						((Literal) s).getLexicalForm();
-					else if (s.isResource()) {
-						Resource r = (Resource) s;
-						System.out.println(r);
-						if (!r.isAnon()) {
-							r.getURI();
-						}
-					}
-				}
-				System.out.println("==================================");
-				System.out.println("==================================");
-
-				// ========================================
-				/*
-				 * StmtIterator it =
-				 * qexec.getDataset().getDefaultModel().listStatements(); while
-				 * (it.hasNext()) { Statement stmt = it.nextStatement(); // get
-				 * next statement Resource s = stmt.getSubject(); // get the
-				 * subject Property p = stmt.getPredicate(); // get the RDFNode
-				 * o = stmt.getObject(); // get the object if (s instanceof
-				 * Resource) { System.out.println(" object( " + s.toString() +
-				 * " )"); } }
-				 */
-
-			} finally {
-				dataset.end();
-			}
-		}
-		// resolve answer
-		return result;
-	}
-
-	/**
-	 * 
-	 */
-	@Override
-	public String makeSPARQLReq(String query) {
-		dataset.begin(ReadWrite.READ);
-
-		try (QueryExecution qExec = QueryExecutionFactory.create(query, dataset)) {
-			ResultSet rs = qExec.execSelect();
-			ResultSetFormatter.out(rs);
-		} finally {
-			dataset.end();
-		}
-		return null;
+	public static void main(String[] args) {
+		TDBManager a = new TDBManager();
+		a.createOrConnectTDB("TestFolder", "myTDB");
+		// Insert SCHEMA into database
+		File f = new File("RDF_Files/jade-hs_VORAUSSETZUNGEN_instance.rdf");
+		//
+		// System.out.println(f.getAbsoluteFile().getAbsolutePath());
+		a.insertDataFromFile(f, Lang.RDFXML);
+		// a.makeSPARQLReq("SELECT  *  {?s ?p ?o} ");
+		TDBPrinter.printModelContent(a.getDataset());
+		a.resolveCompoundTEST1();
 	}
 }
