bbacbe961ab441fba935abaa7ca378b09f4fca43
Philipp-Geoinformatik <kickphilipp@gmail.com>
2016-12-27 17:06:58 +0100

    Reading all links of a person and pushing it to a List<String>.  next step: Recursion with each subelement of "hatFreund"  for testing possible issues with recursion.

--- TripleStoreDB/src/de/jhs/oldenburg/gg/owl/tdb/manager/OWLTDBManager.java
+++ TripleStoreDB/src/de/jhs/oldenburg/gg/owl/tdb/manager/OWLTDBManager.java
@@ -2,21 +2,24 @@ package de.jhs.oldenburg.gg.owl.tdb.manager;
 
 import java.io.File;
 import java.util.Date;
-import java.util.List;
+import java.util.Vector;
 
 import org.apache.jena.ontology.OntModel;
 import org.apache.jena.ontology.OntModelSpec;
 import org.apache.jena.query.Dataset;
 import org.apache.jena.query.Query;
+import org.apache.jena.query.QueryException;
 import org.apache.jena.query.QueryExecution;
 import org.apache.jena.query.QueryExecutionFactory;
 import org.apache.jena.query.QueryFactory;
+import org.apache.jena.query.QuerySolution;
 import org.apache.jena.query.ReadWrite;
 import org.apache.jena.query.ResultSet;
-import org.apache.jena.query.ResultSetFormatter;
+import org.apache.jena.rdf.model.Literal;
 import org.apache.jena.rdf.model.Model;
 import org.apache.jena.rdf.model.ModelFactory;
-import org.apache.jena.rdf.model.StmtIterator;
+import org.apache.jena.rdf.model.RDFNode;
+import org.apache.jena.rdf.model.Resource;
 import org.apache.jena.tdb.TDB;
 import org.apache.jena.tdb.TDBFactory;
 import org.apache.jena.vocabulary.DCTerms;
@@ -50,13 +53,11 @@ public class OWLTDBManager {
 	 * Manage all needed functions
 	 */
 	public void run() {
-		System.out.println(new Date().getTime());
-		System.out.println("sdfdsf");
-		System.out.println("sdfdsf");
-		System.out.println("sdfdsf");
-		System.out.println("sdfdsf");
+		System.out.println("////////////////////////////////////////////////////////////");
+		System.out.println("////////////////" + new Date() + "////////////////");
+		System.out.println("////////////////////////////////////////////////////////////");
 		initDataset();
-		// deleteModel();
+		deleteModel();
 		loadGraph(GRAPH_NAME, "I_AM_THE_CREATOR");
 		createOntology("RDF_Files/friends.owl");
 		// TDBPrinter.printModelContent(dataset);
@@ -82,11 +83,9 @@ public class OWLTDBManager {
 
 	public void loadGraph(String graphName, String creator) {
 		Model m = ModelFactory.createDefaultModel();
-
 		m.createResource(graphName).addProperty(RDF.type, OWL.Ontology).addProperty(DCTerms.creator, creator);
 		dataset.addNamedModel(graphName, m);
 		System.out.println(dataset.getDefaultModel().isEmpty());
-		System.out.println("dsfds");
 		TDB.sync(m);
 	}
 
@@ -109,35 +108,70 @@ public class OWLTDBManager {
  	 * 
 	 */
 	public void deleteModel() {
-		if (dataset != null)
+		if (dataset != null) {
 			dataset.getNamedModel(GRAPH_NAME).removeAll();
-		else {
+			dataset.getDefaultModel().removeAll();
+		} else {
 			System.out.println("Cannot find any RDF-model!");
 		}
 	}
 
 	public void resolveCompound() {
+		Vector<String> personen = new Vector<>(10);
+
 		dataset.begin(ReadWrite.READ);
 		// ///Query to the Graph model//////
 		// Query query =
 		// QueryFactory.create("select ?o  where{ <file:///C:/Users/Philipp/git/Project-TripleStoreDataBase/RDF_Creation/RDF_Files/Mann> a ?o.}");
-		Query query = QueryFactory.create("SELECT DISTINCT ?property WHERE {  ?s ?property ?o .}");
-		try (QueryExecution qexec = QueryExecutionFactory.create(query, dataset.getNamedModel(GRAPH_NAME))) {
-
+		// Query query = QueryFactory.create("SELECT DISTINCT  ?o " +
+		// "WHERE {<file:///C:/Users/Philipp/git/Project-TripleStoreDataBase/TripleStoreDB/RDF_Files/Mann> a ?o.}");
+		String rdf = "prefix rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#> ";
+		String rdfs = "prefix rdfs: <http://www.w3.org/2000/01/rdf-schema#> ";
+		String foaf = "prefix foaf: <http://xmlns.com/foaf/0.1/> ";
+		String owl = "prefix owl: <http://www.w3.org/2002/07/owl#> ";
+		String jhs = "prefix jhs: <file:///C:/Users/Philipp/git/Project-TripleStoreDataBase/TripleStoreDB/RDF_Files/> ";
+		Query query = QueryFactory.create(rdf + rdfs + foaf + owl + jhs + "SELECT DISTINCT ?s ?p ?o " + "WHERE {<file:///C:/Users/Philipp/git/Project-TripleStoreDataBase/TripleStoreDB/RDF_Files/Gabi>  ?p ?o .}");
+		try (QueryExecution qexec = QueryExecutionFactory.create(query, dataset.getDefaultModel())) {
 			// ///Get result//////
 			ResultSet results = qexec.execSelect();
-			ResultSetFormatter.out(System.out, results);
-			// List<String> rsm = results.getResultVars();
-			// System.out.println(rsm.size());
-			// rsm.forEach(System.out::println);
-			// StmtIterator it = rsm.listStatements();
 			//
-			// while (it.hasNext()) {
-			// Statement type = it.nextStatement();
-			// if (!type.asTriple().getObject().isLiteral())
-			// System.out.println(type.asTriple().getObject().getURI());
-			// }
-
+			while (results.hasNext()) {
+				QuerySolution soln = results.nextSolution();
+				RDFNode o = soln.get("?o");
+				RDFNode p = soln.get("?p");
+				RDFNode s = soln.get("?s");
+				// Subject
+				if (s != null)
+					if (s.isLiteral())
+						//System.out.println("SUBJECTasLITERAL: " + ((Literal) s).getLexicalForm() + " ");
+						System.out.println();
+					else if (s.isResource()) {
+						Resource r = (Resource) s;
+						System.out.print("---> SUBJECT: " + s.toString());
+						if (!r.isAnon()) {
+							r.getURI();
+						}
+					}
+				// Predicate
+				if (p != null)
+					if (p.isResource()) {
+						Resource r3 = (Resource) p;
+						//System.out.print("---> PREDICATE: " + r3.toString());
+					} else {
+					}
+				// Object
+				if (o != null)
+					if (o.isResource()) {
+						Resource r2 = (Resource) o;
+						//System.out.print(" | OBJECT: " + r2.toString());
+						// System.out.println(r2);
+						personen.add(r2.toString());
+					}
+				//System.out.println();
+			}
+			personen.forEach(System.out::println);
+		} catch (QueryException e) {
+			e.printStackTrace();
 		} catch (Exception e) {
 			e.printStackTrace();
 		} finally {
