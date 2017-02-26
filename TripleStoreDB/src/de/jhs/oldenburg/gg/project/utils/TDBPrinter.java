595d607f186d87c9d1dc68e1eb7098821b7f95c6
Philipp-Geoinformatik <kickphilipp@gmail.com>
2017-02-26 21:35:23 +0100

    Printer extended

--- TripleStoreDB/src/de/jhs/oldenburg/gg/project/utils/TDBPrinter.java
+++ TripleStoreDB/src/de/jhs/oldenburg/gg/project/utils/TDBPrinter.java
@@ -1,17 +1,23 @@
 package de.jhs.oldenburg.gg.project.utils;
 
+import java.util.Iterator;
+
 import org.apache.jena.query.Dataset;
+import org.apache.jena.query.Query;
 import org.apache.jena.query.QueryExecution;
 import org.apache.jena.query.QueryExecutionFactory;
+import org.apache.jena.query.QueryFactory;
 import org.apache.jena.query.ReadWrite;
 import org.apache.jena.query.ResultSet;
 import org.apache.jena.query.ResultSetFormatter;
+import org.apache.jena.query.Syntax;
 import org.apache.jena.rdf.model.Model;
 import org.apache.jena.rdf.model.Property;
 import org.apache.jena.rdf.model.RDFNode;
 import org.apache.jena.rdf.model.Resource;
 import org.apache.jena.rdf.model.Statement;
 import org.apache.jena.rdf.model.StmtIterator;
+import org.apache.jena.sparql.core.Quad;
 
 /**
  * 
@@ -65,7 +71,7 @@ public class TDBPrinter {
 	/**
 	 * 
 	 */
-	public static String printSPARQLReq(String query, Dataset dataset) {
+	public static String printSELECT(String query, Dataset dataset) {
 		dataset.begin(ReadWrite.READ);
 
 		try (QueryExecution qExec = QueryExecutionFactory.create(query, dataset)) {
@@ -77,6 +83,53 @@ public class TDBPrinter {
 		return null;
 	}
 
+	/**
+	 * 
+	 */
+	public static void printCONSTRUCT(Dataset dataset) {
+		String queryString = "CONSTRUCT WHERE { <http://www.jade-hs.de/RDF/Ontology/Wohneinheit#Wohneinheit_1> ?p ?o }";
+		String queryString2 = "SELECT * WHERE { ?s ?p ?o}";
+		dataset.begin(ReadWrite.READ);
+		try {
+			Query query = QueryFactory.create(queryString);
+			QueryExecution qexec = QueryExecutionFactory.create(query, dataset.getDefaultModel());
+			Model resultModel = qexec.execConstruct();
+			qexec.close();
+			try (QueryExecution qexec2 = QueryExecutionFactory.create(queryString2, resultModel)) {
+				ResultSet results = qexec2.execSelect();
+				ResultSetFormatter.out(results);
+			}
+		} finally {
+			dataset.end();
+		}
+	}
+	
+	
+	/**
+	 * 
+	 */
+	public static void printDESCRIBE(Dataset dataset) {
+		String queryString = "DESCRIBE ?Wohnhaus WHERE {?Wohnhaus <http://www.jade-hs.de/RDF/Ontology#hatGebaeudeeinheit> <http://www.jade-hs.de/RDF/Ontology/Wohneinheit#Wohneinheit_1> }";
+		String queryString2 = "SELECT * WHERE { ?s ?p ?o}";
+		dataset.begin(ReadWrite.READ);
+		try {
+			Query query = QueryFactory.create(queryString) ;
+			QueryExecution qexec = QueryExecutionFactory.create(query, dataset.getDefaultModel()) ;
+			Model resultModel = qexec.execDescribe() ;
+			qexec.close() ;
+			try (QueryExecution qexec2 = QueryExecutionFactory.create(queryString2, resultModel)) {
+				ResultSet results = qexec2.execSelect();
+				ResultSetFormatter.out(results);
+			}
+		} finally {
+			dataset.end();
+		}
+	}
+
+	
+	
+	
+	
 	/**
 	 * 
 	 * @param resultSet
